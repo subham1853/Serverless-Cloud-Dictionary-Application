68d863ecdb7af396e8dd9f0a18c4dca0ef5758ae
Subham Bej <subhambej29@gmail.com>
2026-01-24 00:13:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = 'https://pedabumqcd.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
+  const apiUrl = 'https://pedabumqcd.execute-api.us-east-1.amazonaws.com/dev';
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
